a4520bb7ae1c82b06b21100eb94fcae6d4221b32
C Daniel <thatways.c@gmail.com>
2015-11-17 20:56:27 +0800

    remove useless files

--- org-publish.el
+++ org-publish.el
@@ -5,77 +5,80 @@
 
 (setq org-site-path "f:/Code/ogers/")
 
-(setq org-navi-bar "
-<nav class=\"navbar navbar-default\">
-  <div class=\"container-fluid\">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class=\"navbar-header\">
-      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">
-        <span class=\"sr-only\">Toggle navigation</span>
-        <span class=\"icon-bar\"></span>
-        <span class=\"icon-bar\"></span>
-        <span class=\"icon-bar\"></span>
-      </button>
-      <a class=\"navbar-brand\" href=\"#\">Brand</a>
-    </div>
+;;;(setq org-html-preamble "
+;;;<nav class=\"navbar navbar-default\">
+;;;  <div class=\"container-fluid\">
+;;;    <!-- Brand and toggle get grouped for better mobile display -->
+;;;    <div class=\"navbar-header\">
+;;;      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">
+;;;        <span class=\"sr-only\">Toggle navigation</span>
+;;;        <span class=\"icon-bar\"></span>
+;;;        <span class=\"icon-bar\"></span>
+;;;        <span class=\"icon-bar\"></span>
+;;;      </button>
+;;;      <a class=\"navbar-brand\" href=\"#\">Brand</a>
+;;;    </div>
+;;;
+;;;    <!-- Collect the nav links, forms, and other content for toggling -->
+;;;    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+;;;      <ul class=\"nav navbar-nav\">
+;;;        <li class=\"active\"><a href=\"#\">Link <span class=\"sr-only\">(current)</span></a></li>
+;;;        <li><a href=\"#\">Link</a></li>
+;;;        <li class=\"dropdown\">
+;;;          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
+;;;          <ul class=\"dropdown-menu\">
+;;;            <li><a href=\"#\">Action</a></li>
+;;;            <li><a href=\"#\">Another action</a></li>
+;;;            <li><a href=\"#\">Something else here</a></li>
+;;;            <li role=\"separator\" class=\"divider\"></li>
+;;;            <li><a href=\"#\">Separated link</a></li>
+;;;            <li role=\"separator\" class=\"divider\"></li>
+;;;            <li><a href=\"#\">One more separated link</a></li>
+;;;          </ul>
+;;;        </li>
+;;;      </ul>
+;;;      <form class=\"navbar-form navbar-left\" role=\"search\">
+;;;        <div class=\"form-group\">
+;;;          <input type=\"text\" class=\"form-control\" placeholder=\"Search\">
+;;;        </div>
+;;;        <button type=\"submit\" class=\"btn btn-default\">Submit</button>
+;;;      </form>
+;;;      <ul class=\"nav navbar-nav navbar-right\">
+;;;        <li><a href=\"#\">Link</a></li>
+;;;        <li class=\"dropdown\">
+;;;          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
+;;;          <ul class=\"dropdown-menu\">
+;;;            <li><a href=\"#\">Action</a></li>
+;;;            <li><a href=\"#\">Another action</a></li>
+;;;            <li><a href=\"#\">Something else here</a></li>
+;;;            <li role=\"separator\" class=\"divider\"></li>
+;;;            <li><a href=\"#\">Separated link</a></li>
+;;;          </ul>
+;;;        </li>
+;;;      </ul>
+;;;    </div><!-- /.navbar-collapse -->
+;;;  </div><!-- /.container-fluid -->
+;;;</nav>
+;;;")
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
-      <ul class=\"nav navbar-nav\">
-        <li class=\"active\"><a href=\"#\">Link <span class=\"sr-only\">(current)</span></a></li>
-        <li><a href=\"#\">Link</a></li>
-        <li class=\"dropdown\">
-          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
-          <ul class=\"dropdown-menu\">
-            <li><a href=\"#\">Action</a></li>
-            <li><a href=\"#\">Another action</a></li>
-            <li><a href=\"#\">Something else here</a></li>
-            <li role=\"separator\" class=\"divider\"></li>
-            <li><a href=\"#\">Separated link</a></li>
-            <li role=\"separator\" class=\"divider\"></li>
-            <li><a href=\"#\">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class=\"navbar-form navbar-left\" role=\"search\">
-        <div class=\"form-group\">
-          <input type=\"text\" class=\"form-control\" placeholder=\"Search\">
-        </div>
-        <button type=\"submit\" class=\"btn btn-default\">Submit</button>
-      </form>
-      <ul class=\"nav navbar-nav navbar-right\">
-        <li><a href=\"#\">Link</a></li>
-        <li class=\"dropdown\">
-          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
-          <ul class=\"dropdown-menu\">
-            <li><a href=\"#\">Action</a></li>
-            <li><a href=\"#\">Another action</a></li>
-            <li><a href=\"#\">Something else here</a></li>
-            <li role=\"separator\" class=\"divider\"></li>
-            <li><a href=\"#\">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-")
+;;(setq org-export-html-postamble-format 
+;;      '(("en" "<p class=\"postamble\">Last Updated %d. Created by %c</p>")))
 
 (setq org-publish-project-alist
       '(
-        ("org-htmls"                                                  ;Used to export .org file
-         :base-directory "f:/Code/ogers/"                  ;directory holds .org files 
-         :base-extension "org"                                        ;process .org file only    
-         :publishing-directory "f:/Code/ogers/publish-html/"            ;export destination
+        ("org-htmls"                               ;Used to export .org file
+         :base-directory "./"                      ;directory holds .org files 
+         :base-extension "org"                     ;process .org file only    
+         :publishing-directory "./publish-html/"   ;export destination
          :recursive t
          :publishing-function org-html-publish-to-html
-         :html-preamble ,(with-temp-buffer (insert-file-contents "~/.emacs.d/org-templates/html-preamble.html") (buffer-string))
-         :html-postamble ""          ; your personal postamble
+         :html-preamble t 
+         :html-postamble t
          )
         ("org-static"                ;Used to publish static files
-         :base-directory "f:/Code/ogers/publish-html/static"
+         :base-directory "./publish-html/static"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "f:/Code/ogers/publish-html/"
+         :publishing-directory "./publish-html/"
          :recursive t
          :publishing-function org-publish-attachment
          )
@@ -139,4 +142,4 @@ holding export options."
    ;; Postamble.
    (org-html--build-pre/postamble 'postamble info)
    ;; Closing document.
-   "</div></body>\n</html>"))
+   "</div>\n</body>\n</html>"))
